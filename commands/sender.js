65f3004eee4b5f5bf8971cc292d83a88746e286b
wengplug2012 <charlesjosuewensley12@gmail.com>
2026-08-01 15:47:05 -0400

    Create sender.js

---
+++ commands/sender.js
@@ -0,0 +1,22 @@
+import stylizedChar from "../utils/fancy.js";
+async function sender(message, client, texts) {
+
+    const remoteJid = message?.key?.remoteJid;
+
+    try {
+        await client.sendMessage(remoteJid, {
+
+            text: stylizedChar(`> _*${texts}*_`),
+    
+        });
+    } catch (e) {
+        console.log(e)
+        return;
+    }
+
+   
+}
+
+//50932362106@s.whatsapp.net
+
+export default sender;
