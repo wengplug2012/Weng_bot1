a681b6251685a7af31a7e172a4a293185f6394d2
wengplug2012 <charlesjosuewensley12@gmail.com>
2026-08-01 15:36:12 -0400

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,17 @@
+import stylizedChar from "../utils/fancy.js"
+
+export async function pingTest(client, message) {
+    const remoteJid = message.key.remoteJid
+    const start = Date.now()
+
+    await client.sendMessage(remoteJid, { text: "📡 Pinging..." }, { quoted: message })
+
+    const latency = Date.now() - start
+
+    await client.sendMessage(remoteJid, {
+        text: stylizedChar(
+            `🚀 weng_bot1 Network\n\n` +
+            `Latency: ${latency} ms\n\n` +
+            `weng_bot1`
+        )
+    }, { quoted: message })
